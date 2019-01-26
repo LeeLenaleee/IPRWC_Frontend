b0b4b59da01fcb1a104accbe8e1563eefdc37a9c
Jacco van den Berg <jaccoberg2281@gmail.com>
2019-01-26 16:02:00 +0100

    hoop weg gegooid wat ik niet nodig denk te hebben

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { BelastingComponent } from './belasting/belasting.component';
-import { InstellingenComponent } from './instellingen/instellingen.component';
-import { BelastingListComponent } from './belasting/belasting-list/belasting-list.component';
-import { BelastingItemComponent } from './belasting/belasting-list/belasting-item/belasting-item.component';
-import { KostenpostListComponent } from './instellingen/kostenpost-list/kostenpost-list.component';
-import { KostenpostItemComponent } from './instellingen/kostenpost-list/kostenpost-item/kostenpost-item.component';
-import { BtwWijzigenComponent } from './instellingen/btw-wijzigen/btw-wijzigen.component';
 import { UitloggenComponent } from './inloggen-uitloggen/uitloggen/uitloggen.component';
 import {FooterComponent} from './footer/footer.component';
 import {AppRoutingModule} from './app-routing.module';
@@ -23,25 +16,13 @@ import {ErrorInterceptor} from './inloggen-uitloggen/inloggen/error.interceptor'
 import {LoginComponent} from './login/login.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {AlertComponent} from './inloggen-uitloggen/inloggen';
-import { ContactenListComponent } from './contacten/contact-zoeken/contacten-list/contacten-list.component';
-import { InstellingenService } from './instellingen/instellingen.Service';
-
 import {OnkostenModule} from './onkosten/onkosten.module';
-import {ContactenModule} from './contacten/contacten.module';
-import {RapportagesModule} from './rapportages/rapportages.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BelastingComponent,
-    InstellingenComponent,
-    BelastingListComponent,
-    BelastingItemComponent,
-    KostenpostListComponent,
-    KostenpostItemComponent,
-    BtwWijzigenComponent,
     LoginComponent,
     UitloggenComponent,
     FooterComponent,
@@ -53,8 +34,6 @@ import {RapportagesModule} from './rapportages/rapportages.module';
   imports: [
     BrowserModule,
     OnkostenModule,
-    ContactenModule,
-    RapportagesModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
@@ -66,7 +45,6 @@ import {RapportagesModule} from './rapportages/rapportages.module';
     AuthGuard,
     AlertService,
     AuthenticationService,
-    InstellingenService,
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }],
   bootstrap: [AppComponent]
 })
--- src/app/instellingen/kostenpost-list/kostenpost.model.ts
+++
@@ -1,9 +0,0 @@
-export class Kostenpost {
-    public id: number;
-    public kostenpost: string;
-
-    constructor(id: number, kostenpost: string) {
-        this.id = id;
-        this.kostenpost = kostenpost;
-    }
-}
